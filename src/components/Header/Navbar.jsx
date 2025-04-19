a60963f9344f231b52b7ee57a08d801ffd17b78d
JerinXplore <jerinjerin101325@gmail.com>
2025-04-19 09:52:49 +0600

    added navbar and footer

---
+++ src/components/Header/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className="navbar bg-base-100 shadow-sm">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li><a>Listed Books</a></li>
+        
+        <li><a>Pages to Read</a></li>
+      </ul>
+    </div>
+    <a className="btn btn-ghost text-xl  text-gray-700 font-bold">বইপোকা 🐝🐞</a>
+  </div>
+  <div className="navbar-center hidden lg:flex lg:items-center">
+    <ul className="menu menu-horizontal px-1">
+    <button className="btn btn-success text-lg text-gray-700 font-medium">Home</button>
+    <li className='text-lg text-gray-700 font-medium'><a>Listed Books</a></li>
+      <li className='text-lg  font-medium text-slate-700'><a>Pages to read</a></li>
+    </ul>
+  </div>
+  <div className="navbar-end flex items-center gap-2 px-10">
+  <button className="btn btn-success text-lg text-gray-700 font-medium">Sign In</button>
+  <button className="btn btn-active btn-info text-lg text-gray-700 font-medium">Sign Up</button>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Navbar;
